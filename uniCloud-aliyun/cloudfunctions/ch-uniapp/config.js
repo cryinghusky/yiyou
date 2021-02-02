af1a93d9830a5abf3a992872a2f54c6d730fd2ed
cryingHusky <cryingHusky@163.com>
2021-02-02 17:59:05 +0800

    本地调试环境提交

--- uniCloud-aliyun/cloudfunctions/ch-uniapp/config.js
+++ uniCloud-aliyun/cloudfunctions/ch-uniapp/config.js
@@ -1,6 +1,6 @@
 const auth = require('./middleware/auth')
 
-module.exports = initPlugins({
+module.exports = ({
 	debug: true, // 调试模式时，将返回 stack 错误堆栈
 	baseDir: __dirname, // 项目根目录
 	plugin: {}, // 插件配置，可设置是否启用某插件及插件所有参数
